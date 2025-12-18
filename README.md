e3fb3d1b3ac32fb2590194634dc9aef464ded0c4
Hisnaider <hisnaidercampello2@gmail.com>
2025-12-18 18:45:47 -0300

    feat: Update license information in LICENSE and README.md, and add license entry in main.dart

--- README.md
+++ README.md
@@ -88,5 +88,5 @@ Caso queira conversar sobre o projeto, arquitetura, Flutter ou oportunidades pro
 Desenvolvido por **Hisnaider Ribeiro Campello** 🚀
 
 ## License
-
+[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC%20BY--NC--ND%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)
 This project is licensed under the Creative Commons BY-NC-ND 4.0 License.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore: avoid_web_libraries_in_flutter
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/theme/dark_theme.dart';
 import 'package:portfolio/analytics.dart';
@@ -7,6 +8,16 @@ import 'package:portfolio/src/splash/splash_page.dart';
 
 void main() {
   ///debugPrintRebuildDirtyWidgets = true;
+  LicenseRegistry.addLicense(() async* {
+    yield const LicenseEntryWithLineBreaks(
+      ['Hisnaider Ribeiro Campello'],
+      '''
+      Este trabalho está licenciado sob a Licença Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International. 
+      Para visualizar uma cópia desta licença, visite:
+      https://creativecommons.org/licenses/by-nc-nd/4.0/
+      ''',
+    );
+  });
   runApp(const MainApp());
 }
 
