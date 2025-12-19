b66508fb8f71400018203c49232e7745545295c6
Hisnaider <hisnaidercampello2@gmail.com>
2025-12-19 01:40:07 -0300

    Built web
    Branch: main
    Commit: 788d66b517c585278f82560a2a3a3dff2f88f3a4
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -69,7 +69,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ae9190d668e5d68df9b64a93f0556896
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "9caa357a972bfd9ed7d83aa8ef126bfa",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "af1fe1e3512533301b5c2122908dd989",
+"flutter_bootstrap.js": "5df663d9ef8e8fe5b0d3487329e6d017",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
